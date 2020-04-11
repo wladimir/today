e1a926ee841b4619b54ab0d533dc7c4e731ae202
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2020-04-11 19:52:52 +0200

    initial commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,19 @@
+<!--suppress CssInvalidAtRule -->
+<script>
+    import Hello from './Hello.svelte';
+</script>
+
+<svelte:head>
+    <title>Rustappoint</title>
+</svelte:head>
+
+<main>
+    <Hello />
+</main>
+
+<style global>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
+
