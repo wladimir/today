fc92e1298bcc5b6b12c6360cbf3b00c6055fab16
Vladimir Ćirković <vladimir.cirkovic@rms.com>
2020-04-18 19:02:25 +0200

    added theme switcher

--- frontend/src/Hello.svelte
+++ frontend/src/Hello.svelte
@@ -6,6 +6,12 @@
 
     import { onMount } from 'svelte';
 
+    let d = document.documentElement;
+    let themeStorage = localStorage.getItem('theme');
+    if (themeStorage === 'dark') {
+        d.classList.add('dark-theme');
+    }
+
     let text = 'abc';
 
     let list = ['First Item', 'Second Item', 'Third Item'];
@@ -13,6 +19,16 @@
     const sortList = ev => {list = ev.detail;};
 
     const API_BASE_URL = 'http://localhost:3000/';
+
+    const handleClick = () => {
+        if (d.classList.contains('theme-dark')) {
+            d.classList.remove('theme-dark');
+            localStorage.removeItem('theme');
+        } else {
+            d.classList.add('theme-dark');
+            localStorage.setItem('theme', 'dark');
+        }
+    };
 
     // onMount(async () => {
     //     await fetch(API_BASE_URL)
@@ -29,10 +45,13 @@
 
 <h1>{text}</h1>
 <Icon data={beer} />
-<div class="font-bold text-4xl text-primary_color">
+<div class="font-bold text-4xl text-primary">
     <SortableList
             {list}
             on:sort={sortList}
     />
 </div>
+<button class="theme-btn font-body rounded-btn text-xl font-medium bg-primary hover:bg-secondary text-primary hover:text-secondary px-6 py-3"
+        on:click={handleClick}>Change theme
+</button>
 
