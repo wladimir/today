71d65f017afc8ebcf5366a5b2f39ff3173e641f4
Vladimir Ćirković <vladimir.cirkovic@rms.com>
2020-04-21 21:50:44 +0200

    tailwind will go away

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,9 +1,33 @@
 <!--suppress CssInvalidAtRule -->
 <script>
-    import Hello from './Hello.svelte';
-    import Login from './Login.svelte';
     import Header from './Header.svelte';
     import MainView from './MainView.svelte';
+
+    import { THEME } from './Constants';
+
+    import { onMount } from 'svelte';
+
+    let d = document.documentElement;
+    let themeStorage;
+
+    const handleClick = () => {
+        if (d.classList.contains('theme-dark')) {
+            d.classList.remove('theme-dark');
+            localStorage.removeItem(THEME);
+        } else {
+            d.classList.add('theme-dark');
+            localStorage.setItem(THEME, 'dark');
+            themeStorage = 'dark';
+        }
+    };
+
+    onMount(async () => {
+        themeStorage = localStorage.getItem(THEME);
+
+        if (themeStorage === 'dark') {
+            d.classList.add('theme-dark');
+        }
+    });
 </script>
 
 <svelte:head>
@@ -11,9 +35,9 @@
 </svelte:head>
 
 <main>
-    <Header />
+    <Header {handleClick} />
     <MainView />
-    <Hello />
+    <!--    <Hello />-->
     <!--    <Login />-->
 </main>
 
--- frontend/src/Hello.svelte
+++ frontend/src/Hello.svelte
@@ -1,15 +1,10 @@
 <script>
     import Badge from './Badge.svelte';
-    import Item from './Item.svelte';
     import Icon from 'svelte-awesome';
     import beer from 'svelte-awesome/icons/beer';
     import SortableList from './SortableList.svelte';
-    import { THEME } from './Constants';
 
     import { onMount } from 'svelte';
-
-    let d = document.documentElement;
-    let themeStorage;
 
     let text = 'abc';
 
@@ -19,38 +14,14 @@
 
     const API_BASE_URL = 'http://localhost:3000/';
 
-    const handleClick = () => {
-        if (d.classList.contains('theme-dark')) {
-            d.classList.remove('theme-dark');
-            localStorage.removeItem(THEME);
-        } else {
-            d.classList.add('theme-dark');
-            localStorage.setItem(THEME, 'dark');
-            themeStorage = 'dark';
-        }
-    };
-
     onMount(async () => {
-        themeStorage = localStorage.getItem(THEME);
-
-        if (themeStorage === 'dark') {
-            d.classList.add('theme-dark');
-        }
-
         //     await fetch(API_BASE_URL)
         //             .then(data => data.json())
         //             .then(data => text = data['hello']);
     });
 </script>
 
-<style global>
-    div {
-        font-family: 'rubik';
-    }
-</style>
-
 <h1>{text}</h1>
-<h2>{JSON.stringify(localStorage)}</h2>
 <Icon data={beer} />
 <div class="font-bold text-4xl text-primary">
     <SortableList
@@ -58,8 +29,5 @@
             on:sort={sortList}
     />
 </div>
-<button class="theme-btn font-body rounded-btn text-xl font-medium bg-primary hover:bg-secondary text-primary hover:text-secondary px-6 py-3"
-        on:click={handleClick}>Change theme
-</button>
 <Badge />
 
