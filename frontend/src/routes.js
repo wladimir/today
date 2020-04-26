4e64bac0ae7bbf0f242bf486721b94513190f3ef
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2020-04-26 16:26:35 +0200

    routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,9 +1,16 @@
 import Home from './pages/Home.svelte';
+import SignIn from './pages/SignIn.svelte';
+import Error from './pages/Error.svelte';
 
 export default [
   {
     path: '/',
     component: Home,
+    auth: false,
+  },
+  {
+    path: '/signin',
+    component: SignIn,
     auth: false,
   },
   {
