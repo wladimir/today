741b9929a041b1b3333b39bfc3051149bb834d2a
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2020-04-27 22:21:58 +0200

    baby steps

--- backend/migrations/2020-04-14-135837_add_initial_data/down.sql
+++
@@ -1,7 +0,0 @@
--- This file should undo anything in `up.sql`
-
-DELETE FROM tasks CASCADE;
-
-DELETE FROM projects CASCADE;
-
-DELETE FROM users CASCADE;
--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -1,3 +1,9 @@
 pub mod user;
 pub mod projects;
 pub mod tasks;
+pub mod errors;
+
+pub use user::*;
+pub use projects::*;
+pub use tasks::*;
+pub use errors::*;
