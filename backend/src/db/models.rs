dbf7cb481185aaaa7deb10f328c2d8b1f02be091
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2020-04-28 19:39:46 +0200

    updated user model

--- backend/src/db/models.rs
+++ backend/src/db/models.rs
@@ -1,1 +1,9 @@
-// use crate::schema::users;
+use crate::schema::*;
+
+#[derive(Queryable, Serialize, Deserialize, Debug, Clone, PartialEq)]
+pub struct User {
+    pub id: u32,
+    pub username: String,
+    pub email: String,
+    pub password: String,
+}
