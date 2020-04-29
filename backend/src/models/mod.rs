13d878e53be24777fcdd590efd45d7da8a3cf293
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2020-04-29 20:59:53 +0200

    snapshot

--- backend/src/models/mod.rs
+++ backend/src/models/mod.rs
@@ -1,7 +1,9 @@
 pub mod user;
 pub mod project;
 pub mod task;
+pub mod repository;
 
 pub use user::*;
 pub use project::*;
 pub use task::*;
+pub use repository::*;
--- backend/src/models/tasks.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Clone, Debug, PartialEq)]
-pub struct Task {
-    pub id: u32,
-    pub content: String,
-    pub completed: bool,
-}
-
-impl Task {}
