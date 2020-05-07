cb526db70fa9574ecc2fbabdd50dd4c19bac7dc6
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2020-05-07 22:47:52 +0200

    WIP

--- jhipster/src/main/webapp/app/modules/home/home.tsx
+++ jhipster/src/main/webapp/app/modules/home/home.tsx
@@ -4,9 +4,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { connect } from 'react-redux';
-import { Row, Col, Alert } from 'reactstrap';
-
-import { IRootState } from 'app/shared/reducers';
+import { Alert, Col, Row } from 'reactstrap';
 
 export type IHomeProp = StateProps;
 
